aab1c4cc8222ee7f3b6082eb13e09ed5c2622d82
Piotr <piotrantosz@protonmail.ch>
2016-02-09 12:53:56 +0100

    Remove groups

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-
 from .models import PersonalComputer, License
+from django.contrib.auth.models import Group
+
+admin.site.unregister(Group)
 
 admin.site.register(PersonalComputer)
 
